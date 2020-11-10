a4e684e70f81ded64e24a1c89a688c36d51278af
SGiuri <s.giuri@issm.it>
2020-11-10 22:05:25 +0100

    Done

--- word_count.py
+++ word_count.py
@@ -1,2 +1,15 @@
+import re
+
+
 def count_words(sentence):
-    pass
+    all_words = {}
+    clear_sentence = re.sub(f"[^a-zA-Z0-9\']", " ", sentence)
+
+    for word in clear_sentence.split():
+        cleaner_lower_word = word.strip("'").lower()
+        if cleaner_lower_word not in all_words.keys():
+            all_words[cleaner_lower_word] = 1
+        else:
+            all_words[cleaner_lower_word] += 1
+
+    return all_words
